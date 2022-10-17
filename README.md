96ad5682f27b7e5e27470d5b633fb61cc5cf40e8
Kamesuta <kamesuta@gmail.com>
2022-10-18 02:30:56 +0900

    環境構築

---
+++ README.md
@@ -0,0 +1,13 @@
+# PaintCraft Benchmark Bot
+
+## 概要
+
+PaintCraftの負荷テストのため、複数のプレイヤーを自動操作して負荷をかけるためのツールです。
+
+## 使い方
+
+1. `npm install` で依存パッケージをインストール
+2. `npm run build` でビルド
+3. PaintCraftが導入されたPaperサーバーを起動します。  
+`localhost:25565` で起動してください。
+4. `npm run start` でサーバーにBotをログインさせます。
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import mineflayer from 'mineflayer';
+
+const bot = mineflayer.createBot({
+	host: 'localhost', // minecraft server ip
+	username: 'bot1', // minecraft username
+	// password: '12345678' // minecraft password, comment out if you want to log into online-mode=false servers
+	// port: 25565, // only set if you need a port that isn't 25565
+	// version: false, // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+	// auth: 'mojang' // only set if you need microsoft auth, then set this to 'microsoft'
+});
+
+bot.on('chat', (username, message) => {
+	if (username === bot.username) return;
+	bot.chat(message);
+});
+
+// Log errors and kick reasons:
+bot.on('kicked', console.log);
+bot.on('error', console.log);
